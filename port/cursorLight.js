7a0f86b1470546bf287d013e4eeeb443493e8bdd
skazochniik <elisaveta.minina@gmail.com>
2026-04-07 15:13:45 +0200

    Delete port directory

--- port/cursorLight.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("mousemove", e => {
-  document.documentElement.style.setProperty("--x", e.clientX + "px");
-  document.documentElement.style.setProperty("--y", e.clientY + "px");
-});
-
